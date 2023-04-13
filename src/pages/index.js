424b988a56347572792cadc17ed26ff9782169e3
Hammad Shami <H2Shami@gmail.com>
2023-04-12 18:24:58 -0700

    Created new component that has all the characters and limits selections to just 5

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import Head from 'next/head'
 import React, { useState } from 'react';
-//import { CharacterButton } from "./components/CharacterButton";
+import getCharacters from './model.js';
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
@@ -17,27 +17,9 @@ export default function Home() {
         <link rel="icon" href="/Planorant_Logo.ico" />
       </Head>
       <main className={styles.main}>
-          <container style={{display: 'grid', gridTemplateColumns: '25vw 45vw 15vw'}}>
+          <container style={{display: 'grid', gridTemplateColumns: '18vw 45vw 30vw'}}>
           <grid>
-              <PNGButton url = {"/Character Icons/astra.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/breach.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/brimstone.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/cypher.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/fade.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/jett.webp"} char = {true}/>
-              <PNGButton url = {"/Character Icons/kayo.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/killjoy.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/neon.webp"} char = {true}/>
-              <PNGButton url = {"/Character Icons/omen.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/phoenix.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/raze.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/reyna.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/sage.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/skye.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/sova.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/viper.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/wave.png"} char = {true}/>
-              <PNGButton url = {"/Character Icons/yoru.png"} char = {true}/>
+              <CharacterRoster/>
           </grid>
               <container></container>
               <grid>
@@ -55,6 +37,7 @@ export default function Home() {
   )
 }
 
+
 function PNGButton (props) {
     const [clicked, setClick] = useState(false);
 
@@ -65,13 +48,13 @@ function PNGButton (props) {
     return (
         <button
             style={{
-                width: props.char ? '5vw' : '10vw',
-                height: props.char ? '5vw' : '6vw',
+                width: props.char ? '5vw' : '14vw',
+                height: props.char ? '5vw' : '8vw',
                 background: 'darkslategray',
                 border: 'black',
                 outline: 'solid black',
                 cursor: 'pointer',
-                margin: '1vw',
+                margin: '0.5vw',
                 boxShadow: clicked ? '0px 0px 10px 5px rgba(255,0,0,1)' : 'none'
             }}
             onClick={handleClick}
@@ -84,3 +67,86 @@ function PNGButton (props) {
         </button>
     );
 }
+
+
+//This function renders the list of character buttons
+//Toggles between on and off, allowing a maximum selection of 5
+function CharacterRoster() {
+
+    //This function will eventually be changed to access the database to get the list of characters
+    function getCharacters() {
+        let characters;
+        characters = [
+            "/Character Icons/astra.png",
+            "/Character Icons/breach.png",
+            "/Character Icons/brimstone.png",
+            "/Character Icons/cypher.png",
+            "/Character Icons/fade.png",
+            "/Character Icons/jett.webp",
+            "/Character Icons/kayo.png",
+            "/Character Icons/killjoy.png",
+            "/Character Icons/neon.webp",
+            "/Character Icons/omen.png",
+            "/Character Icons/phoenix.png",
+            "/Character Icons/raze.png",
+            "/Character Icons/reyna.png",
+            "/Character Icons/sage.png",
+            "/Character Icons/skye.png",
+            "/Character Icons/sova.png",
+            "/Character Icons/viper.png",
+            "/Character Icons/wave.png",
+            "/Character Icons/yoru.png",
+        ];
+        return characters;
+    }
+
+    //Grab the characters
+    const urls = getCharacters();
+
+    const [buttonStates, setButtonStates] = useState(
+        urls.map(() => ({ clicked: false }))
+    );
+
+    const [clickedCount, setclickedCount] = useState(0);
+
+    const handleClick = (index) => {
+        const newButtonStates = [...buttonStates];
+        const clicked = newButtonStates[index].clicked;
+        if (clickedCount < 5 || clicked) {
+            newButtonStates[index] = {
+                ...newButtonStates[index],
+                clicked: !clicked
+            };
+            setclickedCount(clicked ? clickedCount - 1 : clickedCount + 1);
+            setButtonStates(newButtonStates);
+        }
+    };
+
+    return (
+        <div>
+            {urls.map((item, index) => (
+                <button
+                    key={index}
+                    style={{
+                        width: '5vw',
+                        height: '5vw',
+                        background: 'darkslategray',
+                        border: 'black',
+                        outline: 'solid black',
+                        cursor: 'pointer',
+                        margin: '0.5vw',
+                        boxShadow: buttonStates[index].clicked ? '0px 0px 10px 5px rgba(255,0,0,1)' : 'none',
+                    }}
+                    onClick={() => handleClick(index)}
+                >
+                    <img
+                        src={item}
+                        alt="Button image"
+                        style={{ display: 'block', margin: 'auto', maxWidth: '100%', maxHeight: '100%' }}
+                    />
+                </button>
+            ))}
+        </div>
+    );
+}
+
