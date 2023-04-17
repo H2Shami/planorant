887ed2e9521375e5b64dde2a6e1b11573ddb33a2
Hammad Shami <hammad.shami@sjsu.edu>
2023-04-17 16:21:53 -0700

    allowed only 1 map selection at a time and returns which characters and maps are selected

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,51 +23,13 @@ export default function Home() {
           </grid>
               <container></container>
               <grid>
-                  <PNGButton url = {"/Map Icons/Ascent.webp"} char = {false}/>
-                  <PNGButton url = {"/Map Icons/Fracture.webp"} char = {false}/>
-                  <PNGButton url = {"/Map Icons/Haven.webp"} char = {false}/>
-                  <PNGButton url = {"/Map Icons/Icebox.webp"} char = {false}/>
-                  <PNGButton url = {"/Map Icons/Lotus.webp"} char = {false}/>
-                  <PNGButton url = {"/Map Icons/Pearl.avif"} char = {false}/>
-                  <PNGButton url = {"/Map Icons/Split.jpeg"} char = {false}/>
+                  <MapRoster/>
               </grid>
       </container>
           </main>
     </>
   )
 }
-
-
-function PNGButton (props) {
-    const [clicked, setClick] = useState(false);
-
-    const handleClick = () => {
-        setClick(!clicked);
-    };
-
-    return (
-        <button
-            style={{
-                width: props.char ? '5vw' : '14vw',
-                height: props.char ? '5vw' : '8vw',
-                background: 'darkslategray',
-                border: 'black',
-                outline: 'solid black',
-                cursor: 'pointer',
-                margin: '0.5vw',
-                boxShadow: clicked ? '0px 0px 10px 5px rgba(255,0,0,1)' : 'none'
-            }}
-            onClick={handleClick}
-        >
-            <img
-                src={props.url}
-                alt="Button image"
-                style={{ display: 'block', margin: 'auto', maxWidth: '100%', maxHeight: '100%' }}
-            />
-        </button>
-    );
-}
-
 
 //This function renders the list of character buttons
 //Toggles between on and off, allowing a maximum selection of 5
@@ -80,8 +42,10 @@ function CharacterRoster() {
             "/Character Icons/astra.png",
             "/Character Icons/breach.png",
             "/Character Icons/brimstone.png",
+            "/Character Icons/Chamber.webp",
             "/Character Icons/cypher.png",
             "/Character Icons/fade.png",
+            "/Character Icons/Gekko.webp",
             "/Character Icons/jett.webp",
             "/Character Icons/kayo.png",
             "/Character Icons/killjoy.png",
@@ -122,6 +86,13 @@ function CharacterRoster() {
         }
     };
 
+    const selectedButtons = buttonStates.reduce((acc, curr, index) => {
+        if (curr.clicked) {
+            acc.push(urls[index]);
+        }
+        return acc;
+    }, []);
+
     return (
         <div>
             {urls.map((item, index) => (
@@ -146,7 +117,86 @@ function CharacterRoster() {
                     />
                 </button>
             ))}
+            <div>
+                Selected Characters: {selectedButtons.join(", ")}
+            </div>
         </div>
     );
 }
 
+function MapRoster() {
+
+    //This function will eventually be changed to access the database to get the list of characters
+    function getMaps() {
+        let maps;
+        maps = [
+            "/Map Icons/Ascent.webp",
+            "/Map Icons/Fracture.webp",
+            "/Map Icons/Haven.webp",
+            "/Map Icons/Icebox.webp",
+            "/Map Icons/Lotus.webp",
+            "/Map Icons/Pearl.avif",
+            "/Map Icons/Split.jpeg",
+        ];
+        return maps;
+    }
+
+    //Grab the characters
+    const urls = getMaps();
+
+    const [buttonStates, setButtonStates] = useState(
+        urls.map(() => ({ clicked: false }))
+    );
+
+    const [clickedCount, setclickedCount] = useState(0);
+
+    const handleClick = (index) => {
+        const newButtonStates = [...buttonStates];
+        const clicked = newButtonStates[index].clicked;
+        if (clickedCount < 1 || clicked) {
+            newButtonStates[index] = {
+                ...newButtonStates[index],
+                clicked: !clicked
+            };
+            setclickedCount(clicked ? clickedCount - 1 : clickedCount + 1);
+            setButtonStates(newButtonStates);
+        }
+    };
+
+    const selectedButtons = buttonStates.reduce((acc, curr, index) => {
+        if (curr.clicked) {
+            acc.push(urls[index]);
+        }
+        return acc;
+    }, []);
+
+    return (
+        <div>
+            {urls.map((item, index) => (
+                <button
+                    key={index}
+                    style={{
+                        width: '14vw',
+                        height: '8vw',
+                        background: 'white',
+                        border: 'black',
+                        outline: 'solid black',
+                        cursor: 'pointer',
+                        margin: '0.5vw',
+                        boxShadow: buttonStates[index].clicked ? '0px 0px 10px 5px rgba(255,0,0,1)' : 'none',
+                    }}
+                    onClick={() => handleClick(index)}
+                >
+                    <img
+                        src={item}
+                        alt="Button image"
+                        style={{ display: 'block', margin: 'auto', maxWidth: '100%', maxHeight: '100%' }}
+                    />
+                </button>
+            ))}
+            <div>
+                Selected Maps: {selectedButtons.join(", ")}
+            </div>
+        </div>
+    );
+}
