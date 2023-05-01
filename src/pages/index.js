3ff46c139ac7aa4a2721c152573d7c123baa6799
mehek2004 <meheksingh84@gmail.com>
2023-05-01 16:19:56 -0700

    secondary option for strategies

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import getCharacters from './model.js';
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
@@ -24,7 +24,7 @@ export default function Home() {
           <grid>
               <CharacterRoster/>
           </grid>
-              <container>{MyCarousel()}</container>
+              <container>{AscentStrats()}</container>
               <grid>
                   <MapRoster/>
               </grid>
@@ -71,10 +71,10 @@ function CharacterRoster() {
     const urls = getCharacters();
 
     const [buttonStates, setButtonStates] = useState(
-        urls.map(() => ({ clicked: false }))
+        urls.map(() => ({ clicked: false, disabled: false }))
     );
 
-    const [clickedCount, setclickedCount] = useState(0);
+    const [clickedCount, setClickedCount] = useState(0);
 
     const handleClick = (index) => {
         const newButtonStates = [...buttonStates];
@@ -84,8 +84,18 @@ function CharacterRoster() {
                 ...newButtonStates[index],
                 clicked: !clicked
             };
-            setclickedCount(clicked ? clickedCount - 1 : clickedCount + 1);
+            setClickedCount(clicked ? clickedCount - 1 : clickedCount + 1);
             setButtonStates(newButtonStates);
+
+            // check if 5 buttons are clicked
+            if (clickedCount === 4 && !clicked) {
+                console.log("character limit reached");
+                // disable the buttons that are not clicked
+                const disabledButtons = newButtonStates.map((buttonState) =>
+                    buttonState.clicked ? { ...buttonState } : { ...buttonState, disabled: true }
+                );
+                setButtonStates(disabledButtons);
+            }
         }
     };
 
@@ -204,7 +214,166 @@ function MapRoster() {
     );
 }
 
-function MyCarousel() {
+// function MyCarousel() {
+//     const customPrevArrow = (onClickHandler, hasPrev, label) => {
+//         return (
+//             hasPrev && (
+//                 <button type="button" onClick={onClickHandler} title={label} style={{ position: 'absolute', zIndex: 2, top: '50%', left: '5%', backgroundColor: 'transparent', borderRadius: '50%', border: 'none', outline: 'none', cursor: 'pointer', transform: 'translate(-50%,-50%)', padding: '10px' }}>
+//                     <span style={{ fontSize: '2rem' }}>&lt;</span>
+//                     <span style={{ fontSize: '1rem', marginLeft: '5px' }}>Prev</span>
+//                 </button>
+//             )
+//         );
+//     };
+//
+//     const customNextArrow = (onClickHandler, hasNext, label) => {
+//         return (
+//             hasNext && (
+//                 <button type="button" onClick={onClickHandler} title={label} style={{ position: 'absolute', zIndex: 2, top: '50%', right: '5%', backgroundColor: 'transparent', borderRadius: '25%', border: 'none', outline: 'none', cursor: 'pointer', transform: 'translate(50%,-50%)', padding: '10px' }}>
+//                     <span style={{ fontSize: '2rem' }}>&gt;</span>
+//                     <span style={{ fontSize: '1rem', marginLeft: '5px' }}>Next</span>
+//                 </button>
+//             )
+//         );
+//     };
+//
+//     return (
+//         <Carousel
+//             renderArrowPrev={customPrevArrow}
+//             renderArrowNext={customNextArrow}
+//             showThumbs={false}
+//         >
+//             <div>
+//                 <MarketSplit />
+//             </div>
+//             <div>
+//                 <img src="/Map Icons/Icebox.webp" alt="Slide 2" />
+//             </div>
+//             <div>
+//                 <img src="/Map Icons/Lotus.webp" alt="Slide 3" />
+//             </div>
+//         </Carousel>
+//     );
+// }
+
+const agents = {
+    omen: {
+        name: 'omen',
+        image: '/Character Icons/omen.png',
+        abilities: [
+            {
+                name: 'smoke',
+                image: "/Ability%20Icons/dark-cover-black.png"
+            },
+            // ...
+        ]
+    },
+    brim:{
+        name: 'brimstone',
+        image: '/Character Icons/brimstone.png',
+        abilities: [
+            {
+                name: 'smoke',
+                image: "/Ability%20Icons/brim.png"
+            }
+        ]
+    }
+    // other agents here...
+};
+
+
+function AscentStrats() {
+    function MarketSplit() {
+        const [image1Position, setImage1Position] = useState({ x: 0, y: 0 });
+        const [image2Position, setImage2Position] = useState({ x:-140, y: 90 });
+        const [image3Position, setImage3Position] = useState({ x: 80, y: 320 });
+        const [image4Position, setImage4Position] = useState({ x: -100, y: 380 });
+
+        useEffect(() => {
+            function handleResize() {
+                const image1 = document.getElementById('image1');
+                const image2 = document.getElementById('image2');
+                const image3 = document.getElementById('image3');
+                const container = document.getElementById('container');
+                const containerRect = container.getBoundingClientRect();
+                const scalingFactor = container.offsetWidth / containerRect.width;
+                const image1Rect = image1.getBoundingClientRect();
+                const image2Rect = image2.getBoundingClientRect();
+                const image3Rect = image3.getBoundingClientRect();
+                setImage1Position({
+                    x: image1Rect.left * scalingFactor,
+                    y: image1Rect.top * scalingFactor,
+                });
+                setImage2Position({
+                    x: (image2Rect.left - image1Rect.left) * scalingFactor,
+                    y: (image2Rect.top - image1Rect.top) * scalingFactor,
+                });
+                setImage3Position({
+                    x: (image3Rect.left - image1Rect.left) * scalingFactor,
+                    y: (image3Rect.top - image1Rect.top) * scalingFactor,
+                });
+            }
+
+            handleResize();
+
+            window.addEventListener('resize', handleResize);
+
+            return () => {
+                window.removeEventListener('resize', handleResize);
+            };
+        }, []);
+
+        return (
+            <div id="container" className="image-container">
+                <img
+                    id="image1"
+                    src="/MiniMap Icons/Ascent.png"
+                    alt="image1"
+                    style={{ position: 'relative' }}
+                />
+                <img
+                    id="image2"
+                    src={agents.brim.abilities[0].image}
+                    alt="smokes"
+                    style={{
+                        position: 'absolute',
+                        top: image1Position.y + image2Position.y,
+                        left: image1Position.x + image2Position.x,
+                        zIndex: 1,
+                        width: '50px',
+                        height: '50px',
+                    }}
+                />
+                <img
+                    id="image3"
+                    src={agents.brim.abilities[0].image}
+                    alt="image3"
+                    style={{
+                        position: 'absolute',
+                        top: image1Position.y + image3Position.y,
+                        left: image1Position.x + image3Position.x,
+                        zIndex: 1,
+                        width: '50px',
+                        height: '50px',
+                    }}
+                />
+                <img
+                    id="image4"
+                    src={agents.brim.image}
+                    alt="image4"
+                    style={{
+                        position: 'absolute',
+                        top: image1Position.y + image4Position.y,
+                        left: image1Position.x + image4Position.x,
+                        zIndex: 1,
+                        width: '50px',
+                        height: '50px',
+                    }}
+                />
+            </div>
+        );
+    }
+
     const customPrevArrow = (onClickHandler, hasPrev, label) => {
         return (
             hasPrev && (
@@ -226,7 +395,6 @@ function MyCarousel() {
             )
         );
     };
-
     return (
         <Carousel
             renderArrowPrev={customPrevArrow}
@@ -234,10 +402,10 @@ function MyCarousel() {
             showThumbs={false}
         >
             <div>
-                <img src="/Map Icons/Haven.webp" alt="Slide 1" />
+                <MarketSplit />
             </div>
             <div>
-                <img src="/Map Icons/Icebox.webp" alt="Slide 2" />
+                <img src="/Strategies/Ascent/Strat1/Duelist/brim;raze;jett;sova;killjoy.png" alt="Slide 2" />
             </div>
             <div>
                 <img src="/Map Icons/Lotus.webp" alt="Slide 3" />
@@ -245,4 +413,6 @@ function MyCarousel() {
         </Carousel>
     );
 }
+
+
 
