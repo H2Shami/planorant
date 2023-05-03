615597241f9527eba067fbf2fd3e910de8244e8a
Hammad Shami <hammad.shami@sjsu.edu>
2023-05-03 12:38:44 -0700

    handler function makes call to postgresql database to return all characters

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from "@prisma/client"
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+class Character {
+  constructor(name, duelist, controller, initiator, sentinel, path) {
+    this.name = name;
+    this.duelist = duelist;
+    this.controller = controller;
+    this.initiator = initiator;
+    this.sentinel = sentinel;
+    this.path = path;
+  }
+}
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  console.log("HANDLER FUNCTION CALLED!");
+  try {
+    const data = await prisma.Character.findMany();
+    return res.status(200).json(data);
+  }
+  catch (error) {
+    return res.status(500).json(error);
+  }
 }
