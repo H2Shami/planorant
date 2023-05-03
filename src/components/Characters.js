9a785d6937c0f4ab9bf4078b9fea0c39c80fc8e8
Hammad Shami <hammad.shami@sjsu.edu>
2023-05-03 12:39:31 -0700

    adding character class and useless getServerSideProps function (should be in index.js)

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { getCharacters } from '@/model.js';
+import { getCharacters, fetchCharacters } from '@/model.js';
+
+class Character {
+    constructor(name, duelist, controller, initiator, sentinel, path) {
+        this.name = name;
+        this.duelist = duelist;
+        this.controller = controller;
+        this.initiator = initiator;
+        this.sentinel = sentinel;
+        this.path = path;
+    }
+}
 
 //This function renders the list of character buttons
 //Toggles between on and off, allowing a maximum selection of 5
-const Characters = () => {
+const Characters = (props) => {
 
+    console.log(props.characters);
+    console.log("^finished printing props");
 
-    //Grab the characters
     const urls = getCharacters();
 
     const [buttonStates, setButtonStates] = useState(
-        urls.map(() => ({ clicked: false }))
+        urls?.map(() => ({ clicked: false }))
     );
 
     const [clickedCount, setclickedCount] = useState(0);
@@ -28,7 +40,7 @@ const Characters = () => {
         }
     };
 
-    const selectedButtons = buttonStates.reduce((acc, curr, index) => {
+    const selectedButtons = buttonStates?.reduce((acc, curr, index) => {
         if (curr.clicked) {
             acc.push(urls[index]);
         }
@@ -37,7 +49,7 @@ const Characters = () => {
 
     return (
         <div>
-            {urls.map((item, index) => (
+            {urls?.map((item, index) => (
                 <button
                     key={index}
                     style={{
@@ -60,10 +72,22 @@ const Characters = () => {
                 </button>
             ))}
             <div>
-                Selected Characters: {selectedButtons.join(", ")}
+                Selected Characters: {selectedButtons?.join(", ")}
             </div>
         </div>
     );
+}
+
+export async function getServerSideProps() {
+    const response = await fetch(`${process.env.BASE_URL}/api/hello.js`)
+    const data = await response.json();
+    console.log(data);
+    console.log("^serversideprops");
+    return {
+        props: {
+            characters: data
+        },
+    };
 }
 
 export default Characters;
