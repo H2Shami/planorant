5f322c2a9116c2b4b3b771703d7e964ba6af12f7
Hammad Shami <hammad.shami@sjsu.edu>
2023-05-20 16:32:51 -0700

    Fixing db prisma sync issues

--- prisma/migrations/20230511201329_added_strategy_table/migration.sql
+++ prisma/migrations/20230511201329_added_strategy_table/migration.sql
@@ -22,15 +22,16 @@ CREATE TABLE "Map" (
 -- CreateTable
 CREATE TABLE "Strategy" (
     "name" TEXT NOT NULL,
-    "duelist" BOOLEAN NOT NULL DEFAULT false,
-    "controller" BOOLEAN NOT NULL DEFAULT false,
-    "initiator" BOOLEAN NOT NULL DEFAULT false,
-    "sentinel" BOOLEAN NOT NULL DEFAULT false,
+    "character1" TEXT NOT NULL,
+    "character2" TEXT NOT NULL,
+    "character3" TEXT NOT NULL,
+    "character4" TEXT NOT NULL,
+    "character5" TEXT NOT NULL,
     "map" TEXT NOT NULL,
     "explanation" TEXT NOT NULL,
     "path" TEXT NOT NULL,
 
-    CONSTRAINT "Strategy_pkey" PRIMARY KEY ("name","duelist","controller","initiator","sentinel","map")
+    CONSTRAINT "Strategy_pkey" PRIMARY KEY ("name")
 );
 
 -- CreateIndex
